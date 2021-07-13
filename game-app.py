7e6f2d4ba38272f356614549bc474f546db107f8
chigozieokoroafor <48325147+chigozieokoroafor@users.noreply.github.com>
2021-07-13 21:49:58 +0100

    removed some lines of code
    left the classes part incase I'd end up using it

--- game-app.py
+++ game-app.py
@@ -3,14 +3,22 @@
 import random
 import json
 
+class Questions:
+    def __init__(self, questions, options, answers ):
+        self.options = options
+        self.answers = answers
+        self.questions = questions
 
-jsonfile = {
-  "module": {
-    "number": 1,
-    "name": "Mathematics",
-    "questions": 30,
-    "revision": "2017-08-13"
-  },
+    def question(self, questions):
+        self.questions = questions
+        print(self.questions)
+
+    def answers(self):
+        print(" continue")
+
+
+jsonfile = '''{
+
   "questions": [
     {
       "number": 1,
@@ -20,34 +28,31 @@ jsonfile = {
         "0.018",
         "0.18"
       ],
-      "correct_answer": 1
+      "correct_answer": 0.0018
     },
     {
       "number": 2,
-      "question": "The next number in the sequence <b>1, 3, 6, 10, </b> is:",
+      "question": "The next number in the sequence 1, 3, 6, 10,  is:",
       "answers": [
         "12",
         "13",
         "14",
         "15"
       ],
-      "correct_answer": 4
+      "correct_answer": 15
     }
   ]
 }
+'''
 
+count = 0
+data = json.loads(jsonfile)
 
-
-class Questions:
-    def __init__(self, questions, options, answers ):
-        self.options = options
-        self.answers = answers
-        self.questions = questions
-
-    def question(self):
-        print(self.questions)
-
-    def answers(self):
-        print(" continue")
-
-Questions(jsonfile["questions"][1],jsonfile["questions"][1], jsonfile["questions"][1])
+for question in data["questions"]:
+    print((question["question"] ))
+    print("options", ":", question["answers"])
+    answer = float(input(">>"))
+    if answer == question["correct_answer"]:
+        count = count+5
+        print("correct answer")
+        print("your score is ", count)
