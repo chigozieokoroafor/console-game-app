7d51bd59df1f43807b9796e4f4081bdee879fe94
chigozieokoroafor <okoroaforc14@gmail.com>
2021-07-25 20:27:46 -0700

    Update game-app.py

--- game-app.py
+++ game-app.py
@@ -1,59 +1,52 @@
 import json
 
 
-def questioneer(data, x):
+def questioneer(data, x=None, score=None):
     while x != len(data["questions"]):
         question_position = data["questions"][x]
         print(question_position["question"])
         print(question_position["options"])
-        ans = input(">> ").upper()
+        answer = input(">> ").upper()
+        question_position = data["questions"][x]
+        if answer == question_position["correct"]:
+            score = score + 5
+            print("correct", score)
+        elif answer != question_position["correct"]:
+            print("wrong", score)
         x = x + 1
-    return ans
+        
+    return score, x
 
 
-def scorer(data, answer, x, score):
-    question_position = data["questions"][x]
-    if answer == question_position["correct"]:
-        score = score+5
-        print("correct", score)
-    elif answer != question_position["correct"]:
-        print("wrong", score)
-    return score
+def log(name, data, score_log, checkpoint_log):
 
+    if name in score_log:
+        answer = questioneer(data, x=checkpoint_log[name], score=score_log[name])
 
-
-def log(name, data, x, score_log, checkpoint_log):
-    if  name in score_log:
-        answer = questioneer(data, checkpoint_log[name])
     elif name not in score_log:
-        answer = questioneer(data, x=0)
+        answer = questioneer(data, x, score)
+
     return answer
 
-def quit(name,quit, x, score,score_log, checkpoint_log):
-    if quit =="Y":
+
+def quit(name, x, score, score_log, checkpoint_log):
+    quit = input("quit>> ").upper()
+    if quit == "Y":
         score_log[name] = score
         checkpoint_log[name] = x
     elif quit == "N":
         pass
 
 
-
 file = open("questions.json")
 
 json_file = json.load(file)
 
-
-
-name_score_log = {}
-name_checkpoint_log = {}
+name_score_log = {"m":5}
+name_checkpoint_log = {"m":5}
 
 score = 0
 x = 0
 name = input("name: ")
-answer = log(name, json_file, x,name_score_log, name_checkpoint_log)
-score =  scorer(json_file, answer, x, score)
-
-
-
-
+answer = log(name, json_file, name_score_log, name_checkpoint_log)
 
